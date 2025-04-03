16c0fb6b9653303c6a5f552cdfaa12db22a7650b
pmdurenr <165648159+usuarioo30@users.noreply.github.com>
2025-04-03 16:23:03 +0200

    implementacion crear solicitud (mejorarlo)

--- Frontend_Angular/src/app/component/interfaz-usuario/list-solicitudes/list-solicitudes.component.ts
+++ Frontend_Angular/src/app/component/interfaz-usuario/list-solicitudes/list-solicitudes.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { SolicitudDescansoService } from '../../../services/solicitud-descanso.service';
-import { SolicitudDescanso } from '../../../model/SolicitudDescanso';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { SolicitudDescanso } from '../../../interfaces/solicitud-descanso';
 
 @Component({
   selector: 'app-list-solicitudes',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './list-solicitudes.component.html',
   styleUrl: './list-solicitudes.component.css'
 })
-export class ListSolicitudesComponent implements OnInit{
-  public solicitudesDescanso: SolicitudDescanso[] = [];
+export class ListSolicitudesComponent implements OnInit {
+  solicitudesDescanso: SolicitudDescanso[] = [];
+  formSolicitudDescanso!: FormGroup;
+  mostrarModal: boolean = false;
+
 
   constructor(
-    private solicitudDescansoService: SolicitudDescansoService
-
-  ) { }
+    private solicitudDescansoService: SolicitudDescansoService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {
+    this.formSolicitudDescanso = this.fb.group({
+      usuario: [this.solicitudDescansoService.getUserNameFromToken(), [Validators.required]],
+      fecha_inicio: ['', [Validators.required]],
+      fecha_fin: ['', [Validators.required]],
+      fecha_solicitada: ['', [Validators.required]]
+    }
+    )
+  }
 
   ngOnInit(): void {
+    const username = this.solicitudDescansoService.getUserNameFromToken();
+    console.log('Username obtenido del token:', username);  // Verifica qué se obtiene aquí
+
+    if (username) {
+      this.formSolicitudDescanso.patchValue({ usuario: username });
+    } else {
+      console.error('No se pudo obtener el nombre del usuario desde el token');
+    }
+
     this.findAllSolicitudesDescanso();
   }
 
@@ -28,4 +52,20 @@ export class ListSolicitudesComponent implements OnInit{
     );
   }
 
+
+  guardarSolicitud() {
+    if (this.formSolicitudDescanso.valid) {
+      const nuevaSolicitud: SolicitudDescanso = this.formSolicitudDescanso.value;
+      this.solicitudDescansoService.saveSolicitudDescanso(nuevaSolicitud).subscribe(
+        (response) => {
+          console.log('Solicitud guardada', response);
+
+          this.findAllSolicitudesDescanso();
+        },
+        (error) => {
+          console.error('Error al guardar la solicitud', error);
+        }
+      );
+    }
+  }
 }
--- Frontend_Angular/src/app/services/solicitud-descanso.service.ts
+++ Frontend_Angular/src/app/services/solicitud-descanso.service.ts
@@ -1,18 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { SolicitudDescanso } from '../model/SolicitudDescanso';
 import { HttpClient } from '@angular/common/http';
+import { SolicitudDescanso } from '../interfaces/solicitud-descanso';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SolicitudDescansoService {
 
-  private urlApi = "http://localhost:5000/requests";
+  private urlApi = "http://localhost:5000";
 
   constructor(private http: HttpClient) { }
 
   getAllSolicitudesDescanso(): Observable<SolicitudDescanso[]> {
-    return this.http.get<SolicitudDescanso[]>(this.urlApi);
+    return this.http.get<SolicitudDescanso[]>(`${this.urlApi}/requests`);
+  }
+
+  saveSolicitudDescanso(solicitudDescanso: SolicitudDescanso) {
+    return this.http.post<void>(`${this.urlApi}/registerRequest`, solicitudDescanso);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('access_token');
+  }
+
+  getUserNameFromToken(): string {
+    const token = this.getToken();
+    if (token) {
+      try {
+        const decoded: any = jwtDecode(token);
+        
+        console.log('Token Decodificado:', decoded);
+        
+        return decoded.sub.username;
+      } catch (error) {
+        console.error('Error al decodificar el token', error);
+        return " ";
+      }
+    }
+    return " ";
   }
 }
