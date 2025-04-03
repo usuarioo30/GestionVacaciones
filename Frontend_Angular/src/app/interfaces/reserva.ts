6e2d1146895969799f8c1dbc3a775858de71972d
SoyaR08 <rafa140200@gmail.com>
2025-04-03 09:04:47 +0200

    eliminación de código no reutilizable

--- Frontend_Angular/src/app/interfaces/reserva.ts
+++
@@ -1,9 +0,0 @@
-export interface Reserva {
-    id: number;
-    sala: string;
-    fechaHoraInicio: string;
-    duracion: number;
-    proyectoAsociado: number;
-    descripcion: string;
-    idUsuario: number;
-}
