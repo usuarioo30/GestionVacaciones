46a3b6fd8fb5a592b76b228d10829badfedf23c7
SoyaR08 <rafa140200@gmail.com>
2025-04-29 11:07:08 +0200

    Arreglado muestra de vacaciones en el calendario

--- Frontend_Angular/src/app/component/modal/new-holiday/new-holiday.component.ts
+++ Frontend_Angular/src/app/component/modal/new-holiday/new-holiday.component.ts
@@ -30,7 +30,7 @@ export class NewHolidayComponent {
   submit() {
     if (this.newHolidayForm.valid) {
       const name = this.newHolidayForm.get('name')?.value;
-      const startDate = this.day.year + '-' + this.day.monthIndex + '-' + this.day.number;
+      const startDate = this.day.year + '-' + (this.day.monthIndex + 1) + '-' + this.day.number;
       //alert(name+" "+startDate);
 
       this.service.addNewHoliday(startDate, name).subscribe({
--- Frontend_Angular/src/app/services/new-holiday.service.ts
+++ Frontend_Angular/src/app/services/new-holiday.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable, Signal, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LocalHoliday } from '../interfaces/local-holiday';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class NewHolidayService {
   private url: string = 'http://localhost:5000/api/'
   private http: HttpClient = inject(HttpClient);
   private localHolidaysSignal = signal<LocalHoliday[]>([]);
+  localHolidayInThisMonthSignal = signal<LocalHoliday[]>([]);
   token: string | null = localStorage.getItem('access_token')
 
   getAllHolidays() {
@@ -34,15 +36,20 @@ export class NewHolidayService {
     return this.http.post<{name: string, date: Date}>(`${this.url}local-holiday/add`, {fecha: date, name}, { headers })
   }
 
-  checkIfDateIsLocalHoliday(date: string): Observable<{isHoliday: boolean}> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`
-    })
-    return this.http.get<{isHoliday: boolean}>(`${this.url}local-holiday?date=${date}`, { headers });
+  // Servicio local: ya no subscribe, solo devuelve el observable
+  checkIfDateIsLocalHoliday(date: string): Observable<LocalHoliday[]> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${this.token}` });
+    return this.http.get<LocalHoliday[]>(`${this.url}local-holiday?date=${date}`, { headers });
   }
+
+  
 
   get LocalHolidays(): Signal<LocalHoliday[]> {
     return this.localHolidaysSignal.asReadonly();
+  }
+
+  get LocalHolidayInThisMonth(): Signal<LocalHoliday[]> {
+    return this.localHolidayInThisMonthSignal.asReadonly();
   }
 
 }
