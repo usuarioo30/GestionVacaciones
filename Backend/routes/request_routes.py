cc2b37fd61ee0f39c70f48421f93b2ff3920e8d3
SoyaR08 <rafa140200@gmail.com>
2025-05-14 10:28:05 +0200

    Refactorizacición de endpoints de solicitudes

--- Backend/routes/request_routes.py
+++ Backend/routes/request_routes.py
@@ -24,3 +24,46 @@ def edit_request(id):
 @jwt_required()
 def delete_request(id):
     return solicitud.eliminar_solicitud(id)
+
+@request_bp.route('/manage/<int:id>', methods=['PUT'])
+@jwt_required()
+def handle_request(id):
+    data = request.get_json()
+    return solicitud.manageRequest(id, data)
+
+@request_bp.route('/list', methods=['GET'])
+@jwt_required()
+def list_requests():
+    return solicitud.listar_solicitudes()
+
+@request_bp.route('/list-admin', methods=['GET'])
+@jwt_required()
+def list_admin_request():
+    return solicitud.listar_solicitudes_admin()
+
+
+@request_bp.route('/list-admin/all', methods=['GET'])
+@jwt_required()
+def list_all_admin_request():
+    return solicitud.listar_todas_solicitudes_admin()
+
+@request_bp.route('/<int:user>', methods=['GET'])
+@jwt_required()
+def get_requests_from_a_user(user):
+    return solicitud.getUserRequest(user)
+
+@request_bp.route('/accepted/<int:user>', methods=['GET'])
+@jwt_required()
+def acceptedRequestOfAUser(user):
+    return solicitud.getAcceptedUserRequest(user)
+
+@request_bp.route('/accepted', methods=['GET'])
+@jwt_required()
+def all_accepted_request():
+    return solicitud.getAcceptedRequests()
+
+@request_bp.route('/compare', methods=['POST'])
+@jwt_required()
+def checkRequests():
+    data = request.get_json()
+    return solicitud.compareRequests(data)
